27fdfb50beda84b2f24834a7bf50bb92d4d2a36e
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-07-23 22:26:38 +0300

    passed teh item test

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function Item({ name, category }) {
+  const [className, setClassName] = useState("")
+
+  function handleClick(){
+    return(
+      setClassName("in-cart")
+    )
+  }
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">Add to Cart</button>
     </li>
   );
 }
